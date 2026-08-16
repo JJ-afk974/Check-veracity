6c6c227c3762e58218b26a80fae7248e65daf15b
JJ-afk974 <anton.terrien@gmail.com>
2026-08-16 12:14:07 +0200

    Update Collecte.py

--- Collecte.py
+++ Collecte.py
@@ -12,6 +12,14 @@ stations = [
     ("Seoul", 37.4943, 126.4905, "e1f10a1e78da46f5b10a1e78da96f525", "m"),
     ("New York", 40.761, -73.864, "e1f10a1e78da46f5b10a1e78da96f525", "e"),
     ("Miami", 25.848, -80.242, "e1f10a1e78da46f5b10a1e78da96f525", "e"),
+    ("Austin", 30.162, -97.689, "e1f10a1e78da46f5b10a1e78da96f525", "e"),
+    ("Dallas", 32.846, -96.87, "e1f10a1e78da46f5b10a1e78da96f525", "e"),
+    ("Denver", 39.705, -104.764, "e1f10a1e78da46f5b10a1e78da96f525", "e"),
+    ("Los Angeles", 33.96, -118.4, "e1f10a1e78da46f5b10a1e78da96f525", "e"),
+    ("Chicago", 41.977, -87.905, "e1f10a1e78da46f5b10a1e78da96f525", "e"),
+    ("Houston", 29.634, -95.246, "e1f10a1e78da46f5b10a1e78da96f525", "e"),
+    ("Seattle", 47.441, -122.3, "e1f10a1e78da46f5b10a1e78da96f525", "e"),
+    ("San Francisco", 37.616, -122.389, "e1f10a1e78da46f5b10a1e78da96f525", "e"),
 ]
 
 today = (datetime.now() + timedelta(hours=2)).date()
